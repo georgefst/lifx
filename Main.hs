eb8341a44ad8a5700f30df782bde9459896a281a
Patrick Pelletier <code@funwithsoftware.org>
2015-07-30 02:04:56 -0700

    preliminary get and put methods for header

--- Main.hs
+++ Main.hs
@@ -26,6 +26,14 @@ data Header
     -- , hdrReserved16  :: !Word16
     }
 
+bProtocol    = 0
+bAddressable = 12
+bTagged      = 13
+bOrigin      = 14
+
+bResRequired = 0
+bAckRequired = 1
+
 bounds :: Integral a => Int -> a -> Put
 bounds name bits val =
   when (val >= limit) $ fail (name ++ ": " ++ show val ++ " >= " ++ show bound)
@@ -41,7 +49,11 @@ instance Binary Header where
         hPro = hdrProtocol h
     bounds "origin" 2 hOrg
     bounds "protocol" 12 hPro
-    putWord16le $ hPro + bitBool 12 hAdd + bitBool 13 hTag + (hOrg `shiftL` 14)
+    putWord16le $
+      (hPro `shiftL` bProtocol) +
+      bitBool bAddressable hAdd +
+      bitBool bTagged hTag +
+      (hOrg `shiftL` bOrigin)
     putWord32le $ hdrSource h
     -- "Frame Address"
     putWord64le $ hdrTarget h
@@ -49,9 +61,28 @@ instance Binary Header where
     putWord16le 0
     let hAck = hdrAckRequired h
         hRes = hdrResRequired h
-    putWord8 $ bitBool 0 hRes + bitBool 1 hAck
+    putWord8 $ bitBool bResRequired hRes + bitBool bAckRequired hAck
     putWord8 $ hdrSequence h
     -- "Protocol Header"
     putWord64le 0 -- Reserved64
     putWord16le $ hdrType h
     putWord16le 0 -- Reserved16
+
+  get = do
+    h <- Header <$> getWord16le -- hdrSize
+    otap <- getWord16le
+    let hh = h (extract otap bOrigin 2)
+               (boolBit otap bTagged)
+               (boolBit otap bAddressable)
+               (extract otap bProtocol 12)
+    hhh <- hh <$> getWord16le -- hdrSource
+              <*> getWord64le -- hdrTarget
+    getWord32le -- Reserved48
+    getWord16le
+    ar <- getWord8
+    let hhhh = hhh (boolBit ar bAckRequired) (boolBit ar bResRequired)
+    hhhhh <- hhhh <$> getWord8 -- hdrSequence
+    getWord64 -- Reserved64
+    hhhhhh <- hhhhh <$> getWord16le -- hdrType
+    getWord16 -- Reserved16
+    return hhhhhh
