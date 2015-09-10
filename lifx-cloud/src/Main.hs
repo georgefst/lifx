bdc07a3c8f7941c39a00886efe7cbe62a33652a7
Patrick Pelletier <code@funwithsoftware.org>
2015-09-10 00:13:51 -0700

    start messing around with the cloud

---
+++ lifx-cloud/src/Main.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Network.HTTP.Client
+import Network.HTTP.Client.TLS
+import Data.Aeson
+import Data.Aeson.Encode.Pretty
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as B8
+import qualified Data.ByteString.Lazy as L
+import Data.Char
+import Data.List
+import Data.Maybe
+import System.IO
+
+main = do
+  lifxTokenStr <- readFile "/Users/ppelleti/.lifxToken"
+  let lifxToken = B8.pack $ takeWhile (not . isSpace) lifxTokenStr
+  mgr <- newManager tlsManagerSettings
+  req <- parseUrl "https://api.lifx.com/v1beta1/lights/all"
+  let req' = applyBasicAuth lifxToken "" req
+  resp <- httpLbs req' mgr
+  let lbs = responseBody resp
+      val = (fromJust $ decode lbs) :: Value
+      pretty = encodePretty val
+  L.putStr pretty
+  putStrLn ""
