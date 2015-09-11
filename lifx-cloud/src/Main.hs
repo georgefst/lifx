35e33c37c60e7aaf94d87e5d96de4bdc9eb6dbc3
Patrick Pelletier <code@funwithsoftware.org>
2015-09-11 10:00:28 -0700

    print colors as MaybeColor

--- lifx-cloud/src/Main.hs
+++ lifx-cloud/src/Main.hs
@@ -15,7 +15,7 @@ import Data.List
 import Data.Maybe
 import Data.Monoid
 import qualified Data.Text as T
-import Data.Vector hiding (takeWhile, mapM_)
+import Data.Vector hiding (takeWhile, mapM_, (++))
 import Data.Version
 import System.IO
 
@@ -26,6 +26,7 @@ pkg_name = "lifx-cloud"
 
 cstat _ _ _ = Nothing
 
+{-
 encPretty = encodePretty' (defConfig { confCompare = cmp })
   where cmp = keyOrder ko <> compare
         ko = [ "id", "name", "selector"
@@ -40,6 +41,7 @@ encPretty = encodePretty' (defConfig { confCompare = cmp })
              , "has_color", "has_variable_color_temp"
              , "operation", "results"
              ]
+-}
 
 userAgent = (hUserAgent, B.concat ua)
   where ua = [ pkg_name, "/", B8.pack (showVersion version)
@@ -91,15 +93,11 @@ getColor mgr lifxToken c = do
   resp <- httpLbs req' mgr
   let lbs = responseBody resp
       (Object val) = (fromJust $ decode lbs) :: Value
-      (Just color) = H.lookup "color" val
-      (Just brightness) = H.lookup "brightness" val
-      pretty = encPretty $ object [
-        ("named-color", String c),
-        ("color", color),
-        ("brightness", brightness)
-        ]
-  L.putStr pretty
-  putStrLn ""
+      (Just (Object color)) = H.lookup "color" val
+      (Just (Number h)) = H.lookup "hue" color
+      (Just (Number s)) = H.lookup "saturation" color
+  putStrLn $ T.unpack c ++ " = HSBK (Just " ++ show (round h) ++
+    ") (Just " ++ show (round s) ++ ") Nothing Nothing"
 
 doColor :: Manager -> B.ByteString -> T.Text -> IO ()
 doColor mgr lifxToken c = do
