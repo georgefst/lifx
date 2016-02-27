51b58438723c2ca07683122ed04acf9f0eb40b46
Patrick Pelletier <code@funwithsoftware.org>
2016-02-26 23:00:14 -0800

    starting to morph this into a test

--- lifx-programs/foo/Main.hs
+++ lifx-programs/foo/Main.hs
@@ -23,18 +23,35 @@ st2 = StateTransition
 
 fromRight = either error id
 
+justColor :: Color -> MaybeColor
+justColor = fmap Just
+
+dly = threadDelay 1000000
+
 main = do
   {-
   lc <- openLanConnection defaultLanSettings
   threadDelay 1000000
   -}
+
+  let devs = map (fromRight . fromText) ["d073d5029e03", "d073d502b95f"]
+      sels = map SelDevId devs
+
   lifxTokenStr <- readFile "/Users/ppelleti/.lifxToken"
   let lifxToken = fromRight $ fromText $ T.pack $ takeWhile (not . isSpace) lifxTokenStr
       cs = defaultCloudSettings { csToken = lifxToken }
   lc <- openCloudConnection cs
-  li <- listLights lc [SelAll] needEverything
+  li <- listLights lc sels needEverything
   print li
   putStrLn ""
+
+  setStates lc [(sels, StateTransition { sPower = Just On
+                                       , sColor = justColor $ HSBK 0 0 1 5000
+                                       , sDuration = 0
+                                       })]
+
+  dly
+
   {-
   tr <- setStates lc [([SelAll], st)]
   tr <- togglePower lc [SelAll] 3.0
@@ -48,6 +65,6 @@ main = do
   let scn = fromRight $ fromText "44f42e8c-96fe-4663-a280-a72e65249162"
   tr <- activateScene lc scn 10.0
   -}
-  tr <- cycleLights lc [SelAll] [st, st2]
+  tr <- cycleLights lc sels [st, st2]
   print tr
   closeConnection lc
