84f631848ac92a68cac0d1bd4de2889a737db78a
Patrick Pelletier <code@funwithsoftware.org>
2016-02-27 23:32:28 -0800

    check both possibilities for toggle power

--- lifx-programs/foo/Main.hs
+++ lifx-programs/foo/Main.hs
@@ -209,14 +209,26 @@ testTogglePower :: (Connection c1, Connection c2)
 testTogglePower conn1 conn2 devs step = do
   tr <- knownState conn1 devs step
   let sels = map SelDevId devs
-  step "toggling power"
+
+  step "toggling power (to off)"
   pwrResult <- togglePower conn1 sels 0
   dly
+
   step "listing lights"
   li <- listLights conn2 sels needEverything
   checkColor (zip3 devs (repeat Off) (repeat defaultColor)) li
   checkLabels (tResults tr) li
   checkLabels pwrResult li
+
+  step "toggling power (to on)"
+  pwrResult' <- togglePower conn1 sels 0
+  dly
+
+  step "listing lights"
+  li' <- listLights conn2 sels needEverything
+  checkColor (zip3 devs (repeat On) (repeat defaultColor)) li'
+  checkLabels (tResults tr) li'
+  checkLabels pwrResult' li'
 
 stateFromColor :: MaybeColor -> StateTransition
 stateFromColor c = StateTransition { sPower = Just On
