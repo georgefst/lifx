1475d5e043f7881dd6de8444137bc0f8b2611c87
Patrick Pelletier <code@funwithsoftware.org>
2016-11-12 22:48:24 -0800

    remove unused imports

--- lifx-base/tests/PureTests.hs
+++ lifx-base/tests/PureTests.hs
@@ -2,15 +2,10 @@
 
 -- module PureTests (pureTests) where
 
-import Control.Arrow
-import Control.Concurrent
 import Control.Monad
 import qualified Data.ByteString as B
-import Data.Char
-import Data.List
 import Data.Maybe
 import Data.Monoid
-import Data.Ord
 import qualified Data.Text as T
 import Test.Tasty
 import Test.Tasty.HUnit
